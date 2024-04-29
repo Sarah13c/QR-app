d65f98efb4e3f5d59ce26636b14046eb724dbb43
Sarah13c <sarahcb13@hotmail.com>
2024-04-29 15:50:44 -0500

    Tracking

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as Leaflet from 'leaflet';
+import { Polyline, LatLngExpression } from 'leaflet';
 import { Storage } from '@ionic/storage-angular';
 
 @Component({
@@ -11,8 +12,8 @@ export class Tab3Page {
   map: Leaflet.Map = {} as Leaflet.Map;
   markers: any[] = [];
   filterOptions: string[] = [];
-  selectedFilter: string = 'Todos'; // Valor por defecto para mostrar todos los marcadores
-  markerIcon: any; // Variable para almacenar el icono personalizado
+  selectedFilter: string = 'Todos'; 
+  markerIcon: any; 
 
   constructor(private storage: Storage) {}
 
@@ -26,22 +27,19 @@ export class Tab3Page {
       attribution: 'edupala.com'
     }).addTo(this.map);
 
-    // Crear el icono personalizado
     this.markerIcon = Leaflet.icon({
-      iconUrl: 'https://w7.pngwing.com/pngs/995/841/png-transparent-pin-location-map-icon.png', // Ruta del icono personalizado
-      iconSize: [30, 30], // Tamaño del icono
-      iconAnchor: [20, 40], // Punto de anclaje del icono
-      popupAnchor: [0, -40] // Punto de anclaje del popup
+      iconUrl: 'https://w7.pngwing.com/pngs/995/841/png-transparent-pin-location-map-icon.png',
+      iconSize: [30, 30],
+      iconAnchor: [20, 40],
+      popupAnchor: [0, -40]
     });
   }
 
   async getStorageData() {
     let scanHistory: { data: string; latitude?: number; longitude?: number, date?: string, time?: string }[] = (await this.storage.get('scanHistory')) || [];
-    console.log(scanHistory); 
     if (scanHistory.length > 0) {
       const uniqueOptions: Map<string, { latitude: number, longitude: number }[]> = new Map();
   
-      // Agrupar las ubicaciones por código QR en un Map
       scanHistory.forEach((scan: any) => {
         if (!uniqueOptions.has(scan.data)) {
           uniqueOptions.set(scan.data, []);
@@ -49,10 +47,8 @@ export class Tab3Page {
         uniqueOptions.get(scan.data)?.push({ latitude: scan.latitude || 0, longitude: scan.longitude || 0 });
       });
 
-      // Construir el array de opciones únicas para el select sin tener en cuenta los duplicados.
       this.filterOptions = Array.from(uniqueOptions.keys());
 
-      // Crear los marcadores a partir de las ubicaciones agrupadas
       this.markers = [];
       uniqueOptions.forEach((locations, code) => {
         this.markers.push(...locations.map(location => ({
@@ -61,21 +57,18 @@ export class Tab3Page {
           city: code,
           date: scanHistory.find(scan => scan.data === code)?.date,
           time: scanHistory.find(scan => scan.data === code)?.time
-          
         })));
       });
   
-      this.filterMarkers(); // Aplicar filtro inicial
+      this.filterMarkers(); 
     } else {
       this.markers = [];
       this.filterOptions = [];
     }
   }
   
-
   leafletMap() {
     for (const marker of this.markers) {
-      // Crear el contenido del popup con la ciudad, fecha y hora
       const popupContent = `
         <b>Link: ${marker.city}</b><br>
         Fecha: ${marker.date}<br>
@@ -86,36 +79,58 @@ export class Tab3Page {
         .bindPopup(popupContent)
         .openPopup();
     }
+    this.drawTrackingLines();
   }
-  
+
+  drawTrackingLines() {
+    for (let i = 0; i < this.markers.length - 1; i++) {
+      const startPoint: LatLngExpression = [this.markers[i].lat, this.markers[i].long];
+      const endPoint: LatLngExpression = [this.markers[i + 1].lat, this.markers[i + 1].long];
+      
+      // Calculate intermediate points (you can use any logic here)
+      const intermediatePoints: LatLngExpression[] = [
+        startPoint,
+        [(startPoint[0] + endPoint[0]) / 2, (startPoint[1] + endPoint[1]) / 2],
+        endPoint
+      ];
+
+      const polyline = new Polyline(intermediatePoints, {
+        color: 'blue', // color of the line
+        weight: 3, // weight of the line
+        opacity: 0.5, // opacity of the line
+        smoothFactor: 1 // smoothness of the line
+      }).addTo(this.map);
+    }
+  }
 
   filterMarkers(event?: any) {
     if (event) {
       this.selectedFilter = event.target.value;
     }
-    // Limpiar el mapa antes de agregar los marcadores filtrados
+
     this.map.eachLayer(layer => {
       if (layer instanceof Leaflet.Marker) {
         this.map.removeLayer(layer);
       }
     });
-    // Filtrar marcadores según la opción seleccionada
+
     let filteredMarkers = [];
     if (this.selectedFilter === 'Todos') {
       filteredMarkers = this.markers;
     } else {
       filteredMarkers = this.markers.filter(marker => marker.city === this.selectedFilter);
     }
-    // Agregar marcadores filtrados al mapa
+
     for (const marker of filteredMarkers) {
       const popupContent = `
         <b>Link: ${marker.city}</b><br>
         Fecha: ${marker.date}<br>
         Hora: ${marker.time}
       `;
-      Leaflet.marker([marker.lat, marker.long], { icon: this.markerIcon }).addTo(this.map) // Asignar el icono personalizado al marcador
+      Leaflet.marker([marker.lat, marker.long], { icon: this.markerIcon }).addTo(this.map)
         .bindPopup(popupContent)
         .openPopup();
     }
+    this.drawTrackingLines(); // Redraw tracking lines after filtering
   }
 }
